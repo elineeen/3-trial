614039b552451b0a547991e985eca5f3a0048010
elonar <null>
2021-05-28 16:32:16 +0800

    修改vite配置及readme错位

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
 // https://vitejs.dev/config/
+console.dir(process.env)
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  base:'/3-trial/',
 })
